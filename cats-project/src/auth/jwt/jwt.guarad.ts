a05e2386060b5f8119626e137740173149d24cf4
yhj0121 <boys1541@naver.com>
2022-11-21 17:32:33 +0900

    오류나는 프로젝트 삭제

--- cats-project/src/auth/jwt/jwt.guarad.ts
+++
@@ -1,5 +0,0 @@
-import { Injectable } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
-
-@Injectable() //Authguard는 stategy 자동 실행
-export class jwtAuthGuard extends AuthGuard('jwt') {}
